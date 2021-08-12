ec90757aa97fada3c0944aa05d7d84c9cbd18f35
jinfeng <851406283@qq.com>
2021-08-12 14:22:58 +0800

    update script

--- SpecPush.rb
+++ SpecPush.rb
@@ -1,19 +1,63 @@
 #! /usr/bin/ruby
 
-require 'yaml'
+
+
+# 拉取最新代码
+system('git stash')
+system('git pull --rebase origin')
+
+class Color
+    def self.natural 
+        0
+    end
+    def self.black 
+        30 
+    end
+    def self.red 
+        31 
+    end
+    def self.green 
+        32 
+    end
+    def self.yellow 
+        33 
+    end
+    def self.blue 
+        34 
+    end
+    def self.magenta 
+        35 
+    end
+    def self.cyan 
+        36 
+    end
+    def self.white 
+        37 
+    end
+end
+
+def color_text(text, color = Color.natural)
+    if color == 0
+        return text
+    end
+    return "\033[#{color}m#{text}\033[0m"
+end
 
 cur_path = Dir.pwd
 push_path = cur_path
-relate_dir_path =
+relate_dir_path = ''
+user_custom_version = true
 
 # 检查是否存在 SpecPushFile 文件，如果不存在，那么创建
 if not File::exist?(cur_path + '/PodPushFile')
     system('touch PodPushFile')
     File.open(cur_path + '/PodPushFile', 'w+') do |f|
         f.write("#写入*.podspec所在的相对目录，不写默认会在脚本执行的目录下查找
-        PUSH_DIR_PATH=")
+PUSH_DIR_PATH=
+#是否允许用户自定义版本号，不填或填true将允许用户设置自定义的版本号，而不是自增版本号 
+USER_CUSTOM_VERSION=true")
     end
-    puts 'Create PodPushFile'
+    puts color_text('Create PodPushFile', Color.green) 
 end
 
 File.open(cur_path + '/PodPushFile') do |f|
@@ -25,22 +69,26 @@ File.open(cur_path + '/PodPushFile') do |f|
             value = key_value.last
         end
         puts "key=#{key},value=#{value}"
-        if key.to_s == 'PUSH_PATH' and not value.nil?
-            relate_dir_path = value.to_s
-            push_path = cur_path + relate_dir_path
+        if key.to_s == 'PUSH_DIR_PATH' and not value.nil?
+            relate_dir_path = value.to_s.gsub("\n", '')
+            push_path = cur_path + '/' + relate_dir_path
+            # puts "Find releate dir path=#{relate_dir_path}"
+        elsif key.to_s == 'USER_CUSTOM_VERSION' and not value.nil?
+            user_custom_version = value
+            # puts "Find custom version config=#{user_custom_version}"
         end
     end
 end
 
-puts "Push path is: #{push_path}, relate dir path is: #{relate_dir_path}"
+# puts "Push path is: #{push_path}, relate dir path is: #{relate_dir_path}"
 
 # 搜索podspec路径
 podspec_path = ''
-Dir::glob((relate_dir_path.nil? ? '' : (relate_dir_path + '/')) + '*.podspec') do |f|
+Dir::glob((relate_dir_path.length == 0 ? '' : (relate_dir_path + '/')) + '*.podspec') do |f|
     podspec_path = f
 end
 if not File::exist?(podspec_path)
-    puts "Can't find any podspec file in path: #{podspec_path}, please modify PodPushFile"
+    puts color_text "Can't find any podspec file in path: #{podspec_path}, please modify PodPushFile' PUSH_DIR_PATH(key)", Color.red
     return 
 end
 
@@ -64,32 +112,35 @@ File.open(podspec_absolute_path, 'r+') do |f|
     end
 end
 
-puts "Current version is = #{cur_version}"
+puts color_text("Current version = ", Color.white) + color_text("#{cur_version}", Color.green)
 
-puts "Please input pod lib's new version, if there is no input, it will be incremented:"
-input_version = gets.chomp
+# 允许自定义版本号
+if user_custom_version == true
+    puts color_text "Please input pod lib's new version, if there is no input or less than or equal old version, it will be incremented:", Color.white
+    input_version = gets.chomp
 
-# 判断输入的version是否>当前的版本号
-input_v_s = input_version.to_s.split('.')
-cur_v_s = cur_version.split('.')
-# 比较的位置，从最左边开始
-v_index = 0
-# 输入的version是否有效
-input_valid = false
-while v_index < cur_v_s.count && v_index < input_v_s.count do
-    if input_v_s[v_index].to_i > cur_v_s[v_index].to_i
-        # 说明用户输入的version比当前的大
-        input_valid = true
-        break
-    elsif input_v_s[v_index].to_i == cur_v_s[v_index].to_i
-        v_index += 1
-    else
-        break
+    # 判断输入的version是否>当前的版本号
+    input_v_s = input_version.to_s.split('.')
+    cur_v_s = cur_version.split('.')
+    # 比较的位置，从最左边开始
+    v_index = 0
+    # 输入的version是否有效
+    input_valid = false
+    while v_index < cur_v_s.count && v_index < input_v_s.count do
+        if input_v_s[v_index].to_i > cur_v_s[v_index].to_i
+            # 说明用户输入的version比当前的大
+            input_valid = true
+            break
+        elsif input_v_s[v_index].to_i == cur_v_s[v_index].to_i
+            v_index += 1
+        else
+            break
+        end
     end
-end
 
-if input_valid == false 
-    puts "Input invalid version = #{input_version}，will auto +1 in last component"
+    if input_valid == false 
+        puts color_text "Input invalid version = #{input_version}，will auto +1 in last component", Color.natural
+    end
 end
 
 if not File.exist? temp_podspec_absolute_path
@@ -105,9 +156,8 @@ File.open(temp_podspec_absolute_path, 'r+') do |t|
             write_line = line
             version_desc = /.*\.version[\s]*=.*/.match line
             if not version_desc.nil?
-                puts "======> #{version_desc}"
                 version_coms = version_desc.to_s.split('=')
-                if input_valid
+                if input_valid and user_custom_version
                     new_version = input_version.to_s
                 else
                     version_num = version_coms.last.to_s.gsub("'",'')
@@ -122,7 +172,7 @@ File.open(temp_podspec_absolute_path, 'r+') do |t|
                         end
                     end
                 end
-                puts "New version = #{new_version}"
+                puts color_text("New version = ",Color.white) + color_text("#{new_version}", Color.green)
                 write_line = version_coms.first.to_s + '=' + " '#{new_version}'" + "\n"
             end
             t.write write_line  
@@ -130,8 +180,11 @@ File.open(temp_podspec_absolute_path, 'r+') do |t|
     end
 end
 
-puts "Update version frome #{cur_version} to #{new_version}"
+puts "Update version from " + color_text("#{cur_version}",Color.green) + " to " + color_text("#{new_version}", Color.green)
 
 # 将新数据反写回到原始podspec中
 system("cp -f #{temp_podspec_path} #{podspec_path}")
 system("rm -f #{temp_podspec_path}")
+
+system("git stash pop && git commit -am 'update version to #{new_version}'")
+system("git tag #{new_version}")
